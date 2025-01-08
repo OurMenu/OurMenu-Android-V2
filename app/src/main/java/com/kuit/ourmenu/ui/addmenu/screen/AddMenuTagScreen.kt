b68ab92bbf05bfe139a2d564d2d404177f41a670
casperjr <hyunjw0515@gmail.com>
2025-01-08 17:10:23 +0900

    [ui]: 기본 composable, preview 생성(#14)

---
+++ app/src/main/java/com/kuit/ourmenu/ui/addmenu/screen/AddMenuTagScreen.kt
@@ -0,0 +1,16 @@
+package com.kuit.ourmenu.ui.addmenu.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AddMenuTagScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun AddMenuTagScreenPreview() {
+    AddMenuTagScreen()
+}
---
+++ app/src/main/java/com/kuit/ourmenu/ui/common/SearchBar.kt
@@ -0,0 +1,15 @@
+package com.kuit.ourmenu.ui.common
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SearchBar(){
+
+}
+
+@Preview
+@Composable
+private fun SearchBarPreview() {
+    SearchBar()
+}
