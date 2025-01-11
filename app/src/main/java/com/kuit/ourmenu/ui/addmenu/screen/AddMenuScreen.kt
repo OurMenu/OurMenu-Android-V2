532dd7be2e25d1379c211064f50e6cea81e5a247
casperjr <hyunjw0515@gmail.com>
2025-01-11 12:12:26 +0900

    [refactor]: AddMenuSearchScreen의 topappbar 공통 컴포넌트로 수정(#14)

--- app/src/main/java/com/kuit/ourmenu/ui/addmenu/screen/AddMenuScreen.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/addmenu/screen/AddMenuScreen.kt
@@ -11,10 +11,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -23,15 +20,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.kuit.ourmenu.R
 import com.kuit.ourmenu.ui.addmenu.component.AddMenuBottomSheetContent
+import com.kuit.ourmenu.ui.addmenu.component.AddMenuTopAppBar
 import com.kuit.ourmenu.ui.common.SearchBar
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -42,30 +36,13 @@ fun AddMenuScreen(modifier: Modifier = Modifier) {
     BottomSheetScaffold(
         scaffoldState = scaffoldState,
         topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        "OURMENU",
-                        color = Color(0xFFFF5420),
-                        fontWeight = FontWeight.Bold
-                    )
-                },
-                navigationIcon = {
-                    IconButton(onClick = { TODO("뒤로가기 구현") }) {
-                        Icon(
-                            painter = painterResource(R.drawable.ic_top_bar_back),
-                            contentDescription = "Back"
-                        )
-                    }
-                },
-                modifier = Modifier
-                    .drawBehind {
-                        drawRect(
-                            color = Color.Black
-                        )
-                    }
-                    .shadow(elevation = 4.dp)
-            )
+            AddMenuTopAppBar {
+                Text(
+                    "OURMENU",
+                    color = Color(0xFFFF5420),
+                    fontWeight = FontWeight.Bold
+                )
+            }
         },
         sheetContainerColor = Color.White,
         sheetContent = {
