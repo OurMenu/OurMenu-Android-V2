0c8bb5d9bab613390631799b1e437a911b203cfa
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-01-13 20:36:36 +0900

    [refactor](home): 홈 메인 추천 관련 컴포저블 recommendation/main 으로 이동 (#13)

---
+++ app/src/main/java/com/kuit/ourmenu/ui/home/component/recommendation/main/HomeMainRecommendation.kt
@@ -0,0 +1,41 @@
+package com.kuit.ourmenu.ui.home.component.recommendation.main
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeMainRecommendation() {
+    Column(
+        modifier = Modifier
+            .padding(top = 16.dp)
+    ) {
+        HomeMainRecommendationText(
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .width(278.dp)
+                .height(144.dp)
+        )
+
+        HomeMainRecommendationList(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 32.dp)
+                .height(244.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MainRecommendationListPreview() {
+    HomeMainRecommendation()
+}
--- app/src/main/java/com/kuit/ourmenu/ui/home/component/recommendation/main/HomeMainRecommendationText.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/home/component/recommendation/main/HomeMainRecommendationText.kt
@@ -1,37 +1,24 @@
-package com.kuit.ourmenu.ui.home.component
+package com.kuit.ourmenu.ui.home.component.recommendation.main
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kuit.ourmenu.R
 
 @Composable
-fun MainRecommendationText(
+fun HomeMainRecommendationText(
     modifier: Modifier = Modifier,
     imgUrl: String = ""
 ) {
     Image(
-        modifier = Modifier
-            .padding(horizontal = 20.dp)
-            .width(278.dp)
-            .height(144.dp),
+        modifier = modifier,
         painter = painterResource(id = R.drawable.ic_home_reco_1),
         contentDescription = "Home Banner"
     )
 }
 
-@Preview
-@Composable
-private fun MainRecommendationTextPreview() {
-    MainRecommendationText()
-}
