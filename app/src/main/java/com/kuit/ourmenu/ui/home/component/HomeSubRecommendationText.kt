23fb90e2fbbde75669eb69ce2685a78a7098483c
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-01-13 20:17:56 +0900

    [feat](home): 홈 서브 추천 문구 생성 (#13)

---
+++ app/src/main/java/com/kuit/ourmenu/ui/home/component/HomeSubRecommendationText.kt
@@ -0,0 +1,20 @@
+package com.kuit.ourmenu.ui.home.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kuit.ourmenu.R
+import com.kuit.ourmenu.ui.theme.Neutral900
+import com.kuit.ourmenu.ui.theme.ourMenuTypography
+
+
