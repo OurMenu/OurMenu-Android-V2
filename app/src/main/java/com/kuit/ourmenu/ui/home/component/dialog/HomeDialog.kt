5093af92ec816acdf029a6256ca3de08eaefbec4
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-01-16 00:01:21 +0900

    [feat](home): 홈 팝업 추천 문구 추가 (#13)

--- app/src/main/java/com/kuit/ourmenu/ui/home/component/dialog/HomeDialog.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/home/component/dialog/HomeDialog.kt
@@ -8,14 +8,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import com.kuit.ourmenu.ui.theme.Neutral500
+import com.kuit.ourmenu.ui.theme.Neutral900
 import com.kuit.ourmenu.ui.theme.NeutralWhite
+import com.kuit.ourmenu.ui.theme.ourMenuTypography
 
 @Composable
 fun HomePopUpDialog(modifier: Modifier = Modifier) {
@@ -54,6 +60,30 @@ fun HomePopUpDialog(modifier: Modifier = Modifier) {
                         // TODO : Close Click Event
                     }
                 )
+                Text(
+                    modifier = Modifier
+                        .padding(top = 13.dp)
+                        .fillMaxWidth(),
+                    text = "안녕하세요!\n" +
+                            "오늘의 기분은 어떠신가요?", // TODO : 추천 문구 반영
+                    style = ourMenuTypography().pretendard_700_20.copy(
+                        color = Neutral900
+                    ),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    modifier = Modifier
+                        .padding(top = 13.dp)
+                        .fillMaxWidth(),
+                    text = "질문에 답해 기분에 맞는 메뉴를\n" +
+                            "추천받아보세요!",
+                    style = ourMenuTypography().pretendard_500_14.copy(
+                        fontWeight = FontWeight(500),
+                        color = Neutral500
+                    ),
+                    textAlign = TextAlign.Center
+                )
+
             }
         }
     }
