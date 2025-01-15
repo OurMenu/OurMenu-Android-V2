7b0d39ae7afa82ee50103341b42711d46bd0e1b4
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-01-15 23:18:12 +0900

    [feat](home): 홈 팝업 터치! 박스 생성 (#13)

---
+++ app/src/main/java/com/kuit/ourmenu/ui/home/component/dialog/HomeDialog.kt
@@ -0,0 +1,71 @@
+package com.kuit.ourmenu.ui.home.component.dialog
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.kuit.ourmenu.ui.theme.NeutralWhite
+
+@Composable
+fun HomePopUpDialog(modifier: Modifier = Modifier) {
+
+    Dialog(
+        onDismissRequest = {
+            // TODO : Dismiss Dialog Event
+        },
+    ) {
+        Surface(
+            modifier = Modifier
+                .shadow(elevation = 8.dp)
+                .fillMaxWidth()
+                .height(431.dp)
+                .background(
+                    color = NeutralWhite,
+                    shape = RoundedCornerShape(size = 12.dp)
+                )
+                .clip(RoundedCornerShape(12.dp))
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        start = 20.dp,
+                        end = 16.dp,
+                        top = 16.dp,
+                        bottom = 24.dp
+                    )
+            ) {
+                Row {
+                    HomeDialogTouchBox(
+                        modifier = Modifier
+                            .padding(top = 4.dp)
+                            .width(72.dp)
+                            .height(32.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0x111111
+)
+@Composable
+private fun HomePopUpDialogPreview() {
+    HomePopUpDialog()
+}
