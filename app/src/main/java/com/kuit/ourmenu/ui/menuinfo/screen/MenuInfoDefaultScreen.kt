51aa237a7cc1da87ebc8fba9f885aa0da2b13c2a
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-01-16 21:04:48 +0900

    [feat](menu-info): 메뉴 상세 페이지 , 아이콘 화면에 배치 (#23)

--- app/src/main/java/com/kuit/ourmenu/ui/menuinfo/screen/MenuInfoDefaultScreen.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/menuinfo/screen/MenuInfoDefaultScreen.kt
@@ -1,9 +1,40 @@
 package com.kuit.ourmenu.ui.menuinfo.screen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kuit.ourmenu.ui.menuinfo.component.MenuInfoImagePager
+import com.kuit.ourmenu.ui.menuinfo.component.MenuInfoTopIcons
+import com.kuit.ourmenu.ui.menuinfo.dummy.MenuInfoDummyData
 
 @Composable
-fun MenuInfoDefaultScreen(modifier: Modifier = Modifier) {
+fun MenuInfoDefaultScreen() {
 
+    val pagerState = rememberPagerState { 3 }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        MenuInfoImagePager(
+            pagerState = pagerState,
+            pageItems = MenuInfoDummyData.dummyPageDataList
+        )
+
+        MenuInfoTopIcons(
+            onBackClick = {},
+            onVertClick = {}
+        )
+
+
+    }
+}
+
+
+@Preview
+@Composable
+private fun MenuInfoDefaultPreview() {
+    MenuInfoDefaultScreen()
 }
