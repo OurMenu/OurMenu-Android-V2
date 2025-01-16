610c6511cb23bc69e1ffaebbaf50d404b31d182f
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-01-16 22:42:48 +0900

    [feat](common): 메뉴 폴더 칩 컴포넌트 구현 (#23)

---
+++ app/src/main/java/com/kuit/ourmenu/ui/common/MenuFolderChip.kt
@@ -0,0 +1,64 @@
+package com.kuit.ourmenu.ui.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kuit.ourmenu.R
+import com.kuit.ourmenu.ui.theme.Neutral300
+import com.kuit.ourmenu.ui.theme.Neutral700
+import com.kuit.ourmenu.ui.theme.NeutralWhite
+import com.kuit.ourmenu.ui.theme.ourMenuTypography
+
+@Composable
+fun MenuFolderChip(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+    Row(
+        modifier = Modifier
+            .clickable { onClick() }
+            .border(
+                width = 1.dp,
+                color = Neutral300,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .padding(horizontal = 12.dp, vertical = 6.dp)
+    ) {
+        Icon(
+            modifier = Modifier.size(14.dp),
+            painter = painterResource(id = R.drawable.img_dummy_pizza),
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier.padding(start = 8.dp),
+            text = "홍대맛집",
+            style = ourMenuTypography().pretendard_600_32.copy(
+                fontSize = 12.sp,
+                lineHeight = 12.sp,
+                color = Neutral700
+            )
+        )
+    }
+
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+private fun MenuFolderChipPreview() {
+    MenuFolderChip()
+}
