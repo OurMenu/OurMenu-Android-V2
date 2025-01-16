77d6b6283a641708bff683d5a9fb902e24a3b1f4
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-01-16 18:40:08 +0900

    [feat](menu-info): 뒤로가기 , vert 버튼 컴포넌트 구현 (#23)

---
+++ app/src/main/java/com/kuit/ourmenu/ui/menuinfo/screen/MenuInfoDefaultScreen.kt
@@ -0,0 +1,9 @@
+package com.kuit.ourmenu.ui.menuinfo.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MenuInfoDefaultScreen(modifier: Modifier = Modifier) {
+
+}
