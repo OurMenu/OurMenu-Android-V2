c31e260f53a6d12162ff6ca9e240987b4fabd24d
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-01-22 16:00:23 +0900

    [ui]: top bar 세부사항 수정 (#17)

--- app/src/main/java/com/kuit/ourmenu/ui/onboarding/component/OnboardingTopAppBar.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/onboarding/component/OnboardingTopAppBar.kt
@@ -1,13 +1,12 @@
 package com.kuit.ourmenu.ui.onboarding.component
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
@@ -15,6 +14,7 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kuit.ourmenu.R
+import com.kuit.ourmenu.ui.theme.Neutral700
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -25,6 +25,7 @@ fun OnboardingTopAppBar() {
                 imageVector = ImageVector.vectorResource(R.drawable.ic_top_bar_logo),
                 contentDescription = "상단바 로고",
                 tint = Color.Unspecified, // 이거 설정 안하면 검정색으로 보임
+                modifier = Modifier.size(44.dp)
             )
         },
         navigationIcon = {
@@ -32,16 +33,10 @@ fun OnboardingTopAppBar() {
                 Icon(
                     painter = painterResource(R.drawable.ic_top_bar_back),
                     contentDescription = "뒤로가기 버튼",
+                    tint = Neutral700
                 )
             }
         },
-        modifier =
-            Modifier
-                .drawBehind {
-                    drawRect(
-                        color = Color.Black,
-                    )
-                }.shadow(elevation = 4.dp),
     )
 }
 
