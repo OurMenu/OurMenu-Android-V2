f3590ec2bf8420db7e8112dda3e4c544f66d67b0
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-01-25 00:38:22 +0900

    [ui]: signup 비밀번호 입력 ui 구현 완료 (#18)

--- app/src/main/java/com/kuit/ourmenu/ui/onboarding/screen/signup/SignupVerifyScreen.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/onboarding/screen/signup/SignupVerifyScreen.kt
@@ -1,4 +1,4 @@
-package com.kuit.ourmenu.ui.onboarding.screen
+package com.kuit.ourmenu.ui.onboarding.screen.signup
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
