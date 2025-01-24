0c7ae74894bd5571eb069dc4cb884b3da065eec4
casperjr <hyunjw0515@gmail.com>
2025-01-24 21:36:20 +0900

    [fix]: 공통 버튼에 modifier 파라미터 추가

--- app/src/main/java/com/kuit/ourmenu/ui/common/BottomFullWidthButton.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/common/BottomFullWidthButton.kt
@@ -27,6 +27,7 @@ import com.kuit.ourmenu.ui.theme.ourMenuTypography
 
 @Composable
 fun BottomFullWidthButton(
+    modifier : Modifier = Modifier,
     containerColor: Color,
     contentColor: Color,
     text: String,
@@ -34,7 +35,7 @@ fun BottomFullWidthButton(
 ) {
     Button(
         onClick = onClick,
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .size(320.dp, 52.dp)
             .shadow(elevation = 4.dp, shape = RoundedCornerShape(12.dp)),
@@ -55,11 +56,11 @@ fun BottomFullWidthButton(
 @Composable
 private fun BottomFullWidthButtonPreview() {
     Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
-        BottomFullWidthButton(Neutral100, Neutral500, stringResource(R.string.add_restaurant_and_menu_by_myself)){
+        BottomFullWidthButton(containerColor = Neutral100, contentColor = Neutral500, text = stringResource(R.string.add_restaurant_and_menu_by_myself)){
             //onClick 작성
         }
         Spacer(modifier = Modifier.height(16.dp))
-        BottomFullWidthButton(Primary500Main, NeutralWhite, stringResource(R.string.add_menu)){
+        BottomFullWidthButton(containerColor = Primary500Main, contentColor = NeutralWhite, text = stringResource(R.string.add_menu)){
             //onClick 작성
         }
     }
