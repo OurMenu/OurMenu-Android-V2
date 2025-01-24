2d3cdfd5bcb4197f4f5d90cfce827799815387de
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-01-25 00:09:52 +0900

    [feat]: signup 인증코드 입력 ui 구현 완료 (#18)

---
+++ app/src/main/java/com/kuit/ourmenu/ui/onboarding/screen/signup.SignupVerifyScreen.kt
@@ -0,0 +1,178 @@
+package com.kuit.ourmenu.ui.onboarding.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kuit.ourmenu.R
+import com.kuit.ourmenu.ui.common.BottomFullWidthButton
+import com.kuit.ourmenu.ui.onboarding.component.OnboardingTopAppBar
+import com.kuit.ourmenu.ui.onboarding.component.VerifyCodeTextField
+import com.kuit.ourmenu.ui.theme.Neutral300
+import com.kuit.ourmenu.ui.theme.Neutral400
+import com.kuit.ourmenu.ui.theme.Neutral500
+import com.kuit.ourmenu.ui.theme.Neutral900
+import com.kuit.ourmenu.ui.theme.NeutralWhite
+import com.kuit.ourmenu.ui.theme.Primary500Main
+import com.kuit.ourmenu.ui.theme.ourMenuTypography
+
+@Composable
+fun SignupVerifyScreen(modifier: Modifier = Modifier) {
+    val focusRequesters = List(6) { FocusRequester() }
+    val codes: SnapshotStateList<String> = remember { mutableStateListOf("", "", "", "", "", "") }
+
+    // 모든 입력 칸이 채워졌는지 확인
+    val isConfirmButtonEnabled = codes.all { it.isNotEmpty() }
+
+    Scaffold(
+        topBar = {
+            OnboardingTopAppBar()
+        },
+        content = { innerPadding ->
+            Column(
+                modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(20.dp),
+            ) {
+                Text(
+                    text = stringResource(R.string.sent_mail),
+                    style = ourMenuTypography().pretendard_600_24,
+                    color = Neutral900,
+                    modifier = Modifier.padding(top = 104.dp),
+                )
+
+                Row {
+                    Text(
+                        text = stringResource(R.string.in_5_mins),
+                        style = ourMenuTypography().pretendard_700_14,
+                        color = Primary500Main,
+                        modifier = Modifier.padding(top = 4.dp),
+                    )
+
+                    Text(
+                        text = stringResource(R.string.enter_code),
+                        style = ourMenuTypography().pretendard_500_14,
+                        color = Neutral500,
+                        modifier = Modifier.padding(top = 4.dp, start = 4.dp),
+                    )
+                }
+
+                Spacer(Modifier.height(12.dp))
+
+                // TODO: 에러 처리하기
+                Row {
+                    for (i in 0 until 6) {
+                        VerifyCodeTextField(
+                            input = codes[i],
+                            onTextChange = { newText ->
+                                if (newText.length <= 1) {
+                                    codes[i] = newText // Compose에서 상태 변경 감지
+                                }
+                            },
+                            onNext = {
+                                if (i < 5) {
+                                    focusRequesters[i + 1].requestFocus()
+                                }
+                            },
+                            modifier =
+                            Modifier.then(
+                                if (i == 0 && codes[i].isEmpty()) {
+                                    Modifier.focusRequester(focusRequesters[i])
+                                } else {
+                                    Modifier
+                                },
+                            ),
+                            focusRequester = focusRequesters[i],
+                        )
+                        if (i < 5) {
+                            Spacer(modifier = Modifier.size(8.dp))
+                        }
+                    }
+                }
+            }
+        },
+        bottomBar = {
+            Column(
+                modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                ) {
+                    HorizontalDivider(
+                        color = Neutral300,
+                        modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp),
+                    )
+
+                    Box(
+                        contentAlignment = Alignment.Center, // 내용 중앙 정렬
+                        modifier =
+                        Modifier
+                            .background(NeutralWhite)
+                            .height(36.dp)
+                            .width(124.dp),
+                    ) {
+                        Text(
+                            text = stringResource(R.string.didnt_receive),
+                            style = ourMenuTypography().pretendard_500_12,
+                            color = Neutral500,
+                        )
+                    }
+                }
+
+                Text(
+                    text = stringResource(R.string.resend),
+                    style = ourMenuTypography().pretendard_600_14,
+                    color = Primary500Main,
+                    modifier = Modifier.padding(bottom = 36.dp).clickable {
+                        // TODO
+                    },
+                )
+
+                BottomFullWidthButton(
+                    containerColor = if (isConfirmButtonEnabled) Primary500Main else Neutral400,
+                    contentColor = NeutralWhite,
+                    text = stringResource(R.string.confirm),
+                    onClick = {
+                        if (isConfirmButtonEnabled) {
+                            // TODO: Confirm 버튼 동작 추가
+                        }
+                    },
+                )
+            }
+        },
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SignupVerifyScreenPreview() {
+    SignupVerifyScreen()
+}
