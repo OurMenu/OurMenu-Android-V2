3b24100b36155176d38ac8c5dfa097cee8460c9e
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-03 23:30:52 +0900

    [feat](search): SearchMenuScreen 생성 (#29)

---
+++ app/src/main/java/com/kuit/ourmenu/ui/searchmenu/screen/SearchMenuScreen.kt
@@ -0,0 +1,17 @@
+package com.kuit.ourmenu.ui.searchmenu.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SearchMenuScreen(modifier: Modifier = Modifier) {
+
+    
+}
+
+@Preview
+@Composable
+private fun SearchMenuScreenPreview() {
+    SearchMenuScreen()
+}
