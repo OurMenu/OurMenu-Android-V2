b7455935854a7aea59563135a605cefb4e68dd07
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-04 01:17:30 +0900

    [feat](search) 검색 기록 컴포넌트 구현 (#29)

--- app/src/main/java/com/kuit/ourmenu/ui/searchmenu/component/SearchHistory.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/searchmenu/component/SearchHistory.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -34,6 +35,8 @@ fun SearchHistoryList(
     onClick: () -> Unit = {},
     historyList: List<SearchHistoryData>
 ) {
+    val lazyListState = rememberLazyListState()
+
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -48,7 +51,9 @@ fun SearchHistoryList(
                 .padding(bottom = 4.dp)
                 .padding(horizontal = 28.dp)
         )
-        LazyColumn {
+        LazyColumn(
+            state = lazyListState
+        ) {
 
             items(historyList.size) { index ->
                 SearchHistoryItem(
