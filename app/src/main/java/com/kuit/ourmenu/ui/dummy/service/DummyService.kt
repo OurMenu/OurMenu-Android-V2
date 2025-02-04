e5683899e91bb65f8559e8ab8f6feed33ce82ca8
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-05 00:13:52 +0900

    [fix]: RepositoryModule 제거 및 폴더 리팩토링

--- app/src/main/java/com/kuit/ourmenu/ui/dummy/service/DummyService.kt
+++
@@ -1,8 +0,0 @@
-package com.kuit.ourmenu.ui.dummy.service
-
-import com.kuit.ourmenu.ui.dummy.data.DummyResponse
-
-interface DummyService {
-//    @GET("")
-    suspend fun getDummyData(): DummyResponse
-}
