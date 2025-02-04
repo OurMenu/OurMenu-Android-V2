15c6aefdb38d295a752e9ad380af1914c0f113a5
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-04 14:25:45 +0900

    [feat]: SearchMenuScreen 구현 (#29)

--- app/src/main/java/com/kuit/ourmenu/ui/searchmenu/screen/SearchMenuScreen.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/searchmenu/screen/SearchMenuScreen.kt
@@ -1,5 +1,10 @@
 package com.kuit.ourmenu.ui.searchmenu.screen
 
+import android.util.Log
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,24 +13,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kuit.ourmenu.R
-import com.kuit.ourmenu.ui.common.CustomTextField
+import com.kuit.ourmenu.ui.addmenu.component.AddMenuSearchBackground
+import com.kuit.ourmenu.ui.common.GoToMapButton
 import com.kuit.ourmenu.ui.common.SearchTextField
 import com.kuit.ourmenu.ui.common.bottomsheet.BottomSheetDragHandle
 import com.kuit.ourmenu.ui.common.topappbar.OurMenuAddButtonTopAppBar
+import com.kuit.ourmenu.ui.menuinfo.dummy.MenuInfoDummyData
+import com.kuit.ourmenu.ui.searchmenu.component.SearchBottomSheetContent
+import com.kuit.ourmenu.ui.searchmenu.component.SearchHistoryList
 import com.kuit.ourmenu.ui.theme.Neutral300
 import com.kuit.ourmenu.ui.theme.NeutralWhite
 
@@ -34,16 +46,51 @@ import com.kuit.ourmenu.ui.theme.NeutralWhite
 fun SearchMenuScreen(modifier: Modifier = Modifier) {
 
     val scaffoldState = rememberBottomSheetScaffoldState()
+    var showBottomSheet by rememberSaveable { mutableStateOf(false) }
     var dragHandleHeight by remember { mutableStateOf(0.dp) }
-    val bottomSheetContentHeight by remember { mutableStateOf(0.dp) }
+    var bottomSheetPeekHeight by remember { mutableStateOf(0.dp) }
+    var showSearchBackground by rememberSaveable { mutableStateOf(false) }
+    var searchText by rememberSaveable { mutableStateOf("") }
+    var searchActionDone by rememberSaveable { mutableStateOf(false) }
+    val interactionSource = remember { MutableInteractionSource() }
+    val searchBarFocused by interactionSource.collectIsFocusedAsState()
+    val focusManager = LocalFocusManager.current
 
     val density = LocalDensity.current
+
+    LaunchedEffect(searchBarFocused) {
+        if (searchBarFocused) {
+            showSearchBackground = true
+            showBottomSheet = false
+        }
+    }
+
+    BackHandler(enabled = showSearchBackground) {
+        if (searchBarFocused) focusManager.clearFocus()
+        searchActionDone = false
+        showSearchBackground = false
+        searchText = ""
+    }
 
     BottomSheetScaffold(
         scaffoldState = scaffoldState,
         modifier = Modifier.fillMaxWidth(),
         topBar = { OurMenuAddButtonTopAppBar() },
-        sheetContent = { },
+        sheetContent = {
+            SearchBottomSheetContent(
+                modifier = Modifier
+                    .onGloballyPositioned { coordinates ->
+                        val heightPx = coordinates.size.height
+                        bottomSheetPeekHeight = density.run {
+                            heightPx.toDp() + dragHandleHeight
+                        }
+                    },
+                dataList = listOf(
+                    MenuInfoDummyData.dummyData,
+                    MenuInfoDummyData.dummyData,
+                )
+            )
+        },
         sheetContainerColor = NeutralWhite,
         sheetShape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp),
         sheetDragHandle = {
@@ -53,7 +100,7 @@ fun SearchMenuScreen(modifier: Modifier = Modifier) {
                 }
             )
         },
-        sheetPeekHeight = bottomSheetContentHeight,
+        sheetPeekHeight = bottomSheetPeekHeight, // TODO : 아이템 개수에 따라 높이 조절 필요
         containerColor = NeutralWhite,
     ) { innerPadding ->
         Box(
@@ -61,19 +108,37 @@ fun SearchMenuScreen(modifier: Modifier = Modifier) {
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-            Column {
-                SearchTextField(
-                    text = "",
-                    onTextChange = {},
-                    onSearch = {},
-                    borderColor = Neutral300,
-                    placeHolder = R.string.search_place_holder,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 20.dp)
-                        .padding(top = 12.dp)
+            if (!showSearchBackground) {
+                // TODO : 지도
+            } else {
+                SearchHistoryList(
+                    historyList = emptyList()
                 )
             }
+
+            SearchTextField(
+                modifier = Modifier.padding(top = 12.dp, start = 20.dp, end = 20.dp),
+                text = searchText,
+                onTextChange = {
+                    searchText = it
+                    showSearchBackground = true
+                    showBottomSheet = false
+                },
+                placeHolder = R.string.search_place_holder,
+                interactionSource = interactionSource
+            ) {
+                // onSearch 함수
+                if (searchBarFocused) focusManager.clearFocus()
+                searchActionDone = true
+
+            }
+
+            GoToMapButton(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(bottom = 16.dp, end = 20.dp),
+                onClick = { /* TODO : Go To Map Button Click Event */ },
+            )
         }
     }
 
