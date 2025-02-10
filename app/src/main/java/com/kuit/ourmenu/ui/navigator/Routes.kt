917ddb94a89d6615cb027743e8c0f8cec854a691
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-10 19:16:33 +0900

    [fix]: Serialization 의존성 추가 (#35)

--- app/src/main/java/com/kuit/ourmenu/ui/navigator/Routes.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/navigator/Routes.kt
@@ -1,7 +1,11 @@
 package com.kuit.ourmenu.ui.navigator
 
+import kotlinx.serialization.Serializable
+
+
 sealed interface Routes{
     // Home
+    @Serializable
     data object Home: Routes
 
     // 지도
