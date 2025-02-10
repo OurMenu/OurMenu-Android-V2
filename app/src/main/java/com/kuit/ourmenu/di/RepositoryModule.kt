3ae5e4422b32756de1094e90032998a76dd972db
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-02-10 15:33:49 +0900

    [feat]: Repository Module 생성 (#29)

--- app/src/main/java/com/kuit/ourmenu/di/RepositoryModule.kt
+++ app/src/main/java/com/kuit/ourmenu/di/RepositoryModule.kt
@@ -1,18 +1,18 @@
-package com.kuit.ourmenu.di
-
-import com.kuit.ourmenu.dummy.repository.DummyRepository
-import com.kuit.ourmenu.dummy.service.DummyService
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-
-@Module
-@InstallIn(SingletonComponent::class)
-object RepositoryModule {
-
-    @Provides
-    fun provideDummyRepository(dummyService: DummyService): DummyRepository {
-        return DummyRepository(dummyService)
-    }
-}
+//package com.kuit.ourmenu.di
+//
+//import com.kuit.ourmenu.dummy.repository.DummyRepository
+//import com.kuit.ourmenu.dummy.service.DummyService
+//import dagger.Module
+//import dagger.Provides
+//import dagger.hilt.InstallIn
+//import dagger.hilt.components.SingletonComponent
+//
+//@Module
+//@InstallIn(SingletonComponent::class)
+//object RepositoryModule {
+//
+//    @Provides
+//    fun provideDummyRepository(dummyService: DummyService): DummyRepository {
+//        return DummyRepository(dummyService)
+//    }
+//}
