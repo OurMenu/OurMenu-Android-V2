fadff5aa5c12208c3a3c80ecbcf004e4b2de1885
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-02-10 15:49:41 +0900

    [fix]: Repository 에서 @Inject 키워드 제거 (#29)

--- app/src/main/java/com/kuit/ourmenu/dummy/repository/DummyRepository.kt
+++ app/src/main/java/com/kuit/ourmenu/dummy/repository/DummyRepository.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 import javax.inject.Singleton
 
-class DummyRepository @Inject constructor(
+class DummyRepository(
     private val dummyService: DummyService
 ) {
     fun getDummyData(): Flow<DummyData> = flow {
