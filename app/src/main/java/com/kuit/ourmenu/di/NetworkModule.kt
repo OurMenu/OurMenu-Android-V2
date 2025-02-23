117bf0b9c84152cdd0c9064cc08ba2eff9e80507
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-23 20:23:04 +0900

    [feat]: Retrofit 객체 생성 시 client 와 converterFactory 설정 (#39)

--- app/src/main/java/com/kuit/ourmenu/di/NetworkModule.kt
+++ app/src/main/java/com/kuit/ourmenu/di/NetworkModule.kt
@@ -1,6 +1,6 @@
 package com.kuit.ourmenu.di
 
-import com.google.gson.Gson
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.kuit.ourmenu.BuildConfig
 import dagger.Module
 import dagger.Provides
@@ -8,9 +8,10 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -18,13 +19,47 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
+    @OptIn(ExperimentalSerializationApi::class)
+    @Provides
+    @Singleton
+    fun provideJson(): Json =
+        Json {
+            isLenient = true // 유연한 JSON 구분 허용
+            prettyPrint = true // 출력 JSON 을 예쁘게 들여쓰기해서 가독성을 높임
+            encodeDefaults = true // 파라미터의 기본값(default) 을 JSON 으로 인코딩
+            explicitNulls = false // null 값을 명시적으로 표시하지 않음
+            ignoreUnknownKeys = true // JSON 에 정의하지 않은 키가 있어도 무시하고 파싱
+        }
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient =
+        OkHttpClient.Builder().apply {
+            connectTimeout(10, TimeUnit.SECONDS)
+            writeTimeout(10, TimeUnit.SECONDS)
+            readTimeout(10, TimeUnit.SECONDS)
+            addInterceptor(loggingInterceptor)
+        }.build()
+
+    @Provides
+    @Singleton
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
     @Provides
     @Singleton
     fun providesRetrofit(
+        json: Json
     ): Retrofit =
         Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(Gson()))
+            .addConverterFactory(
+                json.asConverterFactory(requireNotNull("application/json".toMediaType()))
+            )
             .build()
 
 }
