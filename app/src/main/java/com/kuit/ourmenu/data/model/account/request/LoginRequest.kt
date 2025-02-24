35862d003f0c77395b4f21a0978ab1d218df52ab
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-02-25 00:17:50 +0900

    [feat]: login POST 요청 및 요청,응답 모델 생성 (#37)

---
+++ app/src/main/java/com/kuit/ourmenu/data/model/account/request/LoginRequest.kt
@@ -0,0 +1,15 @@
+package com.kuit.ourmenu.data.model.account.request
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LoginRequest(
+    @SerialName("email")
+    val email: String,
+    @SerialName("password")
+    val password: String,
+    @SerialName("signInType")
+    val signInType: String
+)
--- app/src/main/java/com/kuit/ourmenu/data/service/AccountService.kt
+++ app/src/main/java/com/kuit/ourmenu/data/service/AccountService.kt
@@ -1,6 +1,8 @@
 package com.kuit.ourmenu.data.service
 
+import com.kuit.ourmenu.data.model.account.request.LoginRequest
 import com.kuit.ourmenu.data.model.account.request.SignupRequest
+import com.kuit.ourmenu.data.model.account.response.LoginResponse
 import com.kuit.ourmenu.data.model.base.BaseResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -10,4 +12,9 @@ interface AccountService {
     fun signup(
         @Body request: SignupRequest
     ): BaseResponse<Unit>
+
+    @POST("api/users/sign-in")
+    fun login(
+        @Body request: LoginRequest
+    ): BaseResponse<LoginResponse>
 }
