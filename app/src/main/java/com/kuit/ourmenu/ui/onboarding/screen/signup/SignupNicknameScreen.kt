0da0330326d4b4a736feed89d1397ccd387ddba1
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-03-05 23:11:02 +0900

    [feat]: 닉네임화면 -> 식사시간 선택 화면으로 변경 (#39)

--- app/src/main/java/com/kuit/ourmenu/ui/onboarding/screen/signup/SignupNicknameScreen.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/onboarding/screen/signup/SignupNicknameScreen.kt
@@ -1,17 +1,32 @@
 package com.kuit.ourmenu.ui.onboarding.screen.signup
 
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -22,8 +37,9 @@ import androidx.navigation.compose.rememberNavController
 import com.kuit.ourmenu.R
 import com.kuit.ourmenu.ui.common.DisableBottomFullWidthButton
 import com.kuit.ourmenu.ui.navigator.Routes
-import com.kuit.ourmenu.ui.onboarding.component.LoginTextField
+import com.kuit.ourmenu.ui.onboarding.component.MealTimeGrid
 import com.kuit.ourmenu.ui.onboarding.component.OnboardingTopAppBar
+import com.kuit.ourmenu.ui.onboarding.model.MealTimeState
 import com.kuit.ourmenu.ui.theme.Neutral500
 import com.kuit.ourmenu.ui.theme.Neutral900
 import com.kuit.ourmenu.ui.theme.ourMenuTypography
@@ -33,15 +49,24 @@ fun SignupNicknameScreen(
     navController: NavController
 ) {
 
-    var nickname by rememberSaveable { mutableStateOf("") }
-    var enable by rememberSaveable { mutableStateOf(false) }
+    val mealTimeList = remember {
+        mutableStateListOf(
+            *List(18) {
+                MealTimeState(mealTime = "${it + 6}:00")
+            }.toTypedArray()
+        )
+    }
+    val selectedTimes = remember { mutableStateListOf<String>() }
+    val enable = selectedTimes.isNotEmpty()
 
     Scaffold(
-        topBar = { OnboardingTopAppBar(
-            onBackClick = {
-                navController.navigateUp()
-            }
-        ) },
+        topBar = {
+            OnboardingTopAppBar(
+                onBackClick = {
+                    navController.navigateUp()
+                }
+            )
+        },
         modifier = Modifier
             .fillMaxSize()
             .imePadding()
@@ -57,27 +82,23 @@ fun SignupNicknameScreen(
                     .padding(top = 92.dp)
             ) {
                 Text(
-                    text = stringResource(R.string.input_nickname),
+                    text = stringResource(R.string.input_meal_time),
                     style = ourMenuTypography().pretendard_600_24,
                     color = Neutral900,
                     modifier = Modifier
                 )
 
                 Text(
-                    text = stringResource(R.string.input_nickname_description),
+                    text = stringResource(R.string.input_meal_time_description),
                     style = ourMenuTypography().pretendard_500_14,
                     color = Neutral500,
                     modifier = Modifier.padding(top = 4.dp),
                 )
 
-                LoginTextField(
-                    modifier = Modifier.padding(top = 12.dp),
-                    placeholder = stringResource(R.string.placeholder_nickname),
-                    input = nickname,
-                    onTextChange = { newValue ->
-                        if (newValue.length <= 10) nickname = newValue
-                        enable = newValue.isNotEmpty()
-                    },
+                MealTimeGrid(
+                    modifier = Modifier.padding(top = 29.dp),
+                    mealTimes = mealTimeList,
+                    selectedTimes = selectedTimes
                 )
             }
 
@@ -95,10 +116,14 @@ fun SignupNicknameScreen(
     }
 }
 
-@Preview
+@Preview(
+    showBackground = true,
+    widthDp = 360
+)
 @Composable
 private fun SignupNicknameScreenPreview() {
     val navController = rememberNavController()
 
     SignupNicknameScreen(navController)
+
 }
