da9a8790c14932bebcf7365c35b3e10e21845f10
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-03-05 23:10:19 +0900

    [feat]: 식사시간 선택 컴포넌트 구현 (#39)

---
+++ app/src/main/java/com/kuit/ourmenu/ui/onboarding/component/MealTime.kt
@@ -0,0 +1,97 @@
+package com.kuit.ourmenu.ui.onboarding.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.kuit.ourmenu.ui.onboarding.model.MealTimeState
+import com.kuit.ourmenu.ui.theme.Neutral100
+import com.kuit.ourmenu.ui.theme.Neutral300
+import com.kuit.ourmenu.ui.theme.Neutral500
+import com.kuit.ourmenu.ui.theme.Primary100
+import com.kuit.ourmenu.ui.theme.Primary500Main
+import com.kuit.ourmenu.ui.theme.ourMenuTypography
+import com.kuit.ourmenu.utils.ViewUtil.noRippleClickable
+
+@Composable
+fun MealTimeGrid(
+    modifier: Modifier = Modifier,
+    mealTimes: SnapshotStateList<MealTimeState>,
+    selectedTimes: SnapshotStateList<String>,
+) {
+    val state = rememberLazyGridState()
+
+    LazyVerticalGrid(
+        modifier = modifier,
+        columns = GridCells.Fixed(3),
+        state = state,
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalArrangement = Arrangement.spacedBy(11.dp),
+    ) {
+        items(18) { index ->
+            MealTimeItem(
+                modifier = Modifier
+                    .width(66.dp)
+                    .height(42.dp),
+                mealTime = mealTimes[index].mealTime,
+                selected = mealTimes[index].selected,
+                updateSelected = { mealTime, selected ->
+                    if (selected) {
+                        if (selectedTimes.size < 4) {
+                            selectedTimes.add(mealTime)
+                            mealTimes[index] = mealTimes[index].copy(selected = true)
+                        }
+                    } else {
+                        selectedTimes.remove(mealTime)
+                        mealTimes[index] = mealTimes[index].copy(selected = false)
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun MealTimeItem(
+    modifier: Modifier = Modifier,
+    mealTime: String = "10:00",
+    selected: Boolean = false,
+    updateSelected: (String, Boolean) -> Unit = { _, _ -> },
+) {
+
+    val containerColor = if (selected) Primary100 else Neutral100
+    val borderColor = if (selected) Primary500Main else Neutral300
+    val textColor = if (selected) Primary500Main else Neutral500
+
+    Box(
+        modifier = modifier
+            .border(
+                width = 1.dp,
+                color = borderColor,
+                shape = RoundedCornerShape(size = 8.dp)
+            )
+            .background(color = containerColor, shape = RoundedCornerShape(size = 8.dp))
+            .noRippleClickable {
+                updateSelected(mealTime, !selected)
+            }
+    ) {
+        Text(
+            text = mealTime,
+            style = ourMenuTypography().pretendard_500_16,
+            color = textColor,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
---
+++ app/src/main/java/com/kuit/ourmenu/ui/onboarding/model/MealTimeState.kt
@@ -0,0 +1,6 @@
+package com.kuit.ourmenu.ui.onboarding.model
+
+data class MealTimeState(
+    val mealTime: String,
+    var selected: Boolean = false
+)
