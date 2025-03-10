2a5ecc8eedad82bbd3dc4086766e0ecab5e5cc1a
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-03-10 19:58:44 +0900

    [ui]: 메뉴 폴더 버튼 UI 수정 (#44)

--- app/src/main/java/com/kuit/ourmenu/ui/menuFolder/component/MenuFolderButton.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/menuFolder/component/MenuFolderButton.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -31,11 +32,9 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.kuit.ourmenu.R
 import com.kuit.ourmenu.ui.theme.Neutral300
 import com.kuit.ourmenu.ui.theme.Neutral700
@@ -171,24 +170,26 @@ fun MenuFolderContent() {
             contentAlignment = Alignment.BottomStart
         ) {
             Column {
-                Image(
-                    painter = painterResource(id = R.drawable.img_popup_dice),
-                    contentDescription = "Folder Icon",
-                    modifier = Modifier.size(32.dp)
-                )
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(
-                    text = "한강뷰 맛집",
-                    color = Color.White,
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Medium
-                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.img_popup_dice),
+                        contentDescription = "Folder Icon",
+                        modifier = Modifier.size(32.dp)
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = "한강 뷰 맛집",
+                        color = NeutralWhite,
+                        style = ourMenuTypography().pretendard_500_24,
+                    )
+                }
                 Spacer(modifier = Modifier.height(2.dp))
                 Text(
                     text = "메뉴 0개",
-                    color = Color.White,
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.Medium
+                    color = NeutralWhite,
+                    style = ourMenuTypography().pretendard_500_14,
                 )
             }
         }
