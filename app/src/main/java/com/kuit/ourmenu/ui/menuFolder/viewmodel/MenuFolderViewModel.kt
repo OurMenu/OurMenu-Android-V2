be6fa4af9a66ab70841feda57cbbaabf40f335f8
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-03-27 21:32:53 +0900

    [feat]: menuFolder ViewModel 작성 (#50)

---
+++ app/src/main/java/com/kuit/ourmenu/ui/menuFolder/viewmodel/MenuFolderViewModel.kt
@@ -0,0 +1,52 @@
+package com.kuit.ourmenu.ui.menuFolder.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kuit.ourmenu.data.model.menuFolder.response.MenuFolderResponse
+import com.kuit.ourmenu.data.repository.MenuFolderRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MenuFolderViewModel @Inject constructor(
+    private val menuFolderRepository: MenuFolderRepository
+): ViewModel() {
+
+    private val _menuFolders = MutableStateFlow<List<MenuFolderResponse>>(emptyList())
+    val menuFolders = _menuFolders.asStateFlow()
+
+    private val _error: MutableStateFlow<String?> = MutableStateFlow(null)
+    val error = _error.asStateFlow()
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading = _isLoading.asStateFlow()
+//    val menuFolders = mutableStateListOf<MenuFolder>()
+//    private val isLoading = mutableStateOf(false)
+//    private val errorMessage = mutableStateOf<String?>(null)
+
+    init {
+        getMenuFolders()
+    }
+
+    fun getMenuFolders() {
+        viewModelScope.launch {
+            _isLoading.value = true
+            _error.value = null
+
+            menuFolderRepository.getMenuFolders()
+                .onSuccess { response ->
+                    if (response != null) {
+                        _menuFolders.value = response.sortedBy { it.index }
+                    }
+                }
+                .onFailure { throwable ->
+                    _error.value = throwable.message ?: "메뉴 폴더를 불러오는 중 오류가 발생했습니다."
+                }
+
+            _isLoading.value = false
+        }
+    }
+}
