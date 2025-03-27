8453de1c1222588af7aae30c11504da9d44dfee2
casperjr <hyunjw0515@gmail.com>
2025-03-28 05:55:46 +0900

    [feat]: 지도 관련 Service 코드들 작성 (#52)

--- app/src/main/java/com/kuit/ourmenu/data/service/MapService.kt
+++ app/src/main/java/com/kuit/ourmenu/data/service/MapService.kt
@@ -1,4 +1,37 @@
 package com.kuit.ourmenu.data.service
 
+import com.kuit.ourmenu.data.model.base.BaseResponse
+import com.kuit.ourmenu.data.model.map.response.MapDetailResponse
+import com.kuit.ourmenu.data.model.map.response.MapMenuDetailResponse
+import com.kuit.ourmenu.data.model.map.response.MapResponse
+import com.kuit.ourmenu.data.model.map.response.MapSearchHistoryResponse
+import com.kuit.ourmenu.data.model.map.response.MapSearchResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface MapService {
+
+    @GET("api/users/menus/{mapId}/maps")
+    suspend fun getMapDetail(
+        @Path("mapId") mapId: Int
+    ): BaseResponse<MapDetailResponse>
+
+    @GET("api/users/menus/maps")
+    suspend fun getMap(): BaseResponse<MapResponse>
+
+    @GET("api/users/menus/maps/{menuId}/search")
+    suspend fun getMapMenuDetail(
+        @Path("menuId") menuId: Int
+    ): BaseResponse<MapMenuDetailResponse>
+
+    @GET("api/users/menus/maps/search")
+    suspend fun getMapSearch(
+        @Query("title") title: String,
+        @Query("mapX") mapX: Double?,
+        @Query("mapY") mapY: Double?
+    ): BaseResponse<MapSearchResponse>
+
+    @GET("api/users/menus/maps/search-history")
+    suspend fun  getMapSearchHistory(): BaseResponse<MapSearchHistoryResponse>
 }
