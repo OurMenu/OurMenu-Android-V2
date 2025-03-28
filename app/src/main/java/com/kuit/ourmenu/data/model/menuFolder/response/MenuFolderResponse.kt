0069c3453c4e1c355f9a1028749199647323a5ea
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-03-28 16:23:57 +0900

    [fix]: api 연결 오류 수정 (#50)

--- app/src/main/java/com/kuit/ourmenu/data/model/menuFolder/response/MenuFolderResponse.kt
+++ app/src/main/java/com/kuit/ourmenu/data/model/menuFolder/response/MenuFolderResponse.kt
@@ -1,5 +1,8 @@
 package com.kuit.ourmenu.data.model.menuFolder.response
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class MenuFolderResponse(
     val menuFolderId: Int,
     val menuFolderTitle: String,
