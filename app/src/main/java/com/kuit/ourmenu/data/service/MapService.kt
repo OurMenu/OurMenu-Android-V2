400a651964d7184b6f2d2c890f59a3a6ca29f064
casperjr <hyunjw0515@gmail.com>
2025-04-03 03:10:47 +0900

    [FEAT]: 크롤링 관련 Service 작성

--- app/src/main/java/com/kuit/ourmenu/data/service/MapService.kt
+++ app/src/main/java/com/kuit/ourmenu/data/service/MapService.kt
@@ -1,6 +1,9 @@
 package com.kuit.ourmenu.data.service
 
 import com.kuit.ourmenu.data.model.base.BaseResponse
+import com.kuit.ourmenu.data.model.map.response.CrawlingHistoryResponse
+import com.kuit.ourmenu.data.model.map.response.CrawlingStoreDetailResponse
+import com.kuit.ourmenu.data.model.map.response.CrawlingStoreInfoResponse
 import com.kuit.ourmenu.data.model.map.response.MapDetailResponse
 import com.kuit.ourmenu.data.model.map.response.MapMenuDetailResponse
 import com.kuit.ourmenu.data.model.map.response.MapResponse
@@ -34,4 +37,21 @@ interface MapService {
 
     @GET("api/users/menus/maps/search-history")
     suspend fun getMapSearchHistory(): BaseResponse<MapSearchHistoryResponse>
+
+    // 크롤링 관련 요청
+    @GET("api/priored/users/{userId}/histories")
+    suspend fun getCrawlingHistory(): BaseResponse<CrawlingHistoryResponse>
+
+    @GET("api/priored/stores/{storeId}")
+    suspend fun getCrawlingStoreDetail(
+        @Query("is-crawled") isCrawled: Boolean,
+        @Path("storeId") storeId: String
+    ): BaseResponse<CrawlingStoreDetailResponse>
+
+    @GET("api/priored/stores/menus")
+    suspend fun getCrawlingStoreInfo(
+        @Query("query") query: String,
+        @Query("mapX") mapX: Double,
+        @Query("mapY") mapY: Double
+    ): BaseResponse<CrawlingStoreInfoResponse>
 }
