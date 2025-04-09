8754c03f98b5ab22adc9d88e32a4aea0144d3058
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-04-09 14:18:34 +0900

    [refactor]: MenuFolderNavigation에 이동 이벤트 작성 (#54)

--- app/src/main/java/com/kuit/ourmenu/ui/menuFolder/navigation/MenuFolderNavigation.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/menuFolder/navigation/MenuFolderNavigation.kt
@@ -1,24 +1,44 @@
 package com.kuit.ourmenu.ui.menuFolder.navigation
 
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.kuit.ourmenu.ui.menuFolder.screen.MenuFolderAllMenuScreen
+import com.kuit.ourmenu.ui.menuFolder.screen.MenuFolderDetailScreen
 import com.kuit.ourmenu.ui.menuFolder.screen.MenuFolderScreen
 import com.kuit.ourmenu.ui.navigator.MainTabRoute
+import com.kuit.ourmenu.ui.navigator.Routes
 
 fun NavController.navigateToMenuFolder(navOptions: NavOptions) {
     navigate(MainTabRoute.MenuFolder, navOptions)
 }
 
+// 이동 이벤트 (menuFolderId 전달)
+fun NavController.navigateToMenuFolderDetail(menuFolderId: Int) {
+    navigate(Routes.MenuFolderDetail(menuFolderId))
+}
+
+fun NavController.navigateToMenuFolderAllMenu() {
+    navigate(Routes.MenuFolderAllMenu)
+}
+
 fun NavGraphBuilder.menuFolderNavGraph(
-    padding: PaddingValues,
-    // navigate 이벤트
+    navigateToMenuFolderDetail: (Int) -> Unit,
+    navigateToMenuFolderAllMenu: () -> Unit,
 ) {
     composable<MainTabRoute.MenuFolder> {
         MenuFolderScreen(
-            // navigate 이벤트 + 기타 이벤트
+            onNavigateToDetail = navigateToMenuFolderDetail,
+            onNavigateToAllMenu = navigateToMenuFolderAllMenu
         )
+
+        composable<Routes.MenuFolderDetail> {
+            MenuFolderDetailScreen()
+        }
+
+        composable<Routes.MenuFolderAllMenu> {
+            MenuFolderAllMenuScreen()
+        }
     }
 }
