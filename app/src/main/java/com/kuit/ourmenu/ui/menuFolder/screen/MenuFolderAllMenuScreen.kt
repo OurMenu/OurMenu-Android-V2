5969cc3fdd8641d025f5a1d91a58430dd5b3f614
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-04-09 20:59:25 +0900

    [refactor]: TagListProvider로 분리 (#54)

--- app/src/main/java/com/kuit/ourmenu/ui/menuFolder/screen/MenuFolderAllMenuScreen.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/menuFolder/screen/MenuFolderAllMenuScreen.kt
@@ -37,6 +37,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kuit.ourmenu.R
 import com.kuit.ourmenu.data.model.menuFolder.response.SortOrderType
+import com.kuit.ourmenu.data.model.menuFolder.response.TagType
 import com.kuit.ourmenu.ui.common.bottomsheet.BottomSheetDragHandle
 import com.kuit.ourmenu.ui.common.topappbar.BackButtonTopAppBar
 import com.kuit.ourmenu.ui.menuFolder.component.AddButton
@@ -50,6 +51,7 @@ import com.kuit.ourmenu.ui.theme.Neutral900
 import com.kuit.ourmenu.ui.theme.NeutralWhite
 import com.kuit.ourmenu.ui.theme.Primary500Main
 import com.kuit.ourmenu.ui.theme.ourMenuTypography
+import com.kuit.ourmenu.utils.TagListProvider
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -63,7 +65,7 @@ fun MenuFolderAllMenuScreen(
     val menuCount = menus.size
 
     var filterCount by rememberSaveable { mutableIntStateOf(0) } // 선택된 필터 개수 상태 관리
-    var selectedFilters by rememberSaveable { mutableStateOf(listOf<String>()) } // 선택된 필터 리스트
+    var selectedFilters by rememberSaveable { mutableStateOf(listOf<TagType>()) } // 선택된 필터 리스트
 
     val scaffoldState = rememberBottomSheetScaffoldState()
     val coroutineScope = rememberCoroutineScope()
@@ -85,29 +87,14 @@ fun MenuFolderAllMenuScreen(
         sheetPeekHeight = 0.dp,
         sheetContent = {
             FilterBottomSheet(
-                categoryTagList = listOf(
-                    R.drawable.ic_tag_rice to "밥",
-                    R.drawable.ic_tag_rice to "빵",
-                    R.drawable.ic_tag_rice to "면"
-                ),
-                nationalityTagList = listOf(
-                    R.drawable.ic_tag_rice to "한식",
-                    R.drawable.ic_tag_rice to "중식",
-                    R.drawable.ic_tag_rice to "일식"
-                ),
-                tasteTagList = listOf(
-                    R.drawable.ic_tag_rice to "매콤함",
-                    R.drawable.ic_tag_rice to "달달함",
-                    R.drawable.ic_tag_rice to "시원함"
-                ),
-                occasionTagList = listOf(
-                    R.drawable.ic_tag_rice to "혼밥",
-                    R.drawable.ic_tag_rice to "친구 약속",
-                    R.drawable.ic_tag_rice to "데이트"
-                ),
+                categoryTagList = TagListProvider.categoryTagList,
+                nationalityTagList = TagListProvider.nationalityTagList,
+                tasteTagList = TagListProvider.tasteTagList,
+                occasionTagList = TagListProvider.occasionTagList,
                 onApplyButtonClick = {
                     coroutineScope.launch {
                         filterCount = selectedFilters.size // 적용 버튼 클릭 시 선택된 필터 개수 반영
+                        viewModel.updateTags(selectedFilters)
                         scaffoldState.bottomSheetState.partialExpand() // 적용 버튼 클릭 시 BottomSheet 닫기
                     }
 
