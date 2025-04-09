00f6c89c317a31f05b35fc898ea113dbb4cfc5b2
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-04-09 13:13:49 +0900

    [feat]: sort order type enum 생성 (#54)

---
+++ app/src/main/java/com/kuit/ourmenu/data/model/menuFolder/response/SortOrderType.kt
@@ -0,0 +1,10 @@
+enum class SortOrderType(val apiValue: String, val displayName: String) {
+    TITLE_ASC("TITLE_ASC", "이름순"),
+//    TITLE_DESC("TITLE_DESC", "이름역순"),
+    CREATED_AT_ASC("CREATED_AT_ASC", "등록순"),
+//    CREATED_AT_DESC("CREATED_AT_DESC", "최신순"),
+//    PRICE_ASC("PRICE_ASC", "낮은 가격순");
+    PRICE_DESC("PRICE_DESC", "가격순");
+
+    override fun toString(): String = displayName
+}
