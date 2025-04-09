ffda1672b850cce63ca66200031caedaa94ba202
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-04-09 13:14:37 +0900

    [feat]: menuFolderDetail service 작성 (#54)

--- app/src/main/java/com/kuit/ourmenu/data/service/MenuFolderService.kt
+++ app/src/main/java/com/kuit/ourmenu/data/service/MenuFolderService.kt
@@ -3,8 +3,16 @@ package com.kuit.ourmenu.data.service
 import com.kuit.ourmenu.data.model.base.BaseResponse
 import com.kuit.ourmenu.data.model.menuFolder.response.MenuFolderResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MenuFolderService {
     @GET("api/menu-folders")
     suspend fun getMenuFolders(): BaseResponse<MenuFolderResponse>
+
+    @GET("api/menu-folders/{menuFolderId}/menus")
+    suspend fun getMenuFolderDetails(
+        @Path("menuFolderId") menuFolderId: Int,
+        @Query("sortOrder") sortOrder: String,
+    ): BaseResponse<MenuFolderResponse>
 }
