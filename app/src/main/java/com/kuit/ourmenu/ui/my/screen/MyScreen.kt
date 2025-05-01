914d1a35675773f9a80afb52d7337ac82d34d800
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-05-01 15:11:08 +0900

    [UI]: 마이페이지 하단 모달 완료 (#43)

--- app/src/main/java/com/kuit/ourmenu/ui/my/screen/MyScreen.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/my/screen/MyScreen.kt
@@ -2,6 +2,7 @@ package com.kuit.ourmenu.ui.my.screen
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,10 +11,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -25,100 +31,114 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kuit.ourmenu.R
 import com.kuit.ourmenu.ui.common.topappbar.OurMenuAddButtonTopAppBar
+import com.kuit.ourmenu.ui.my.component.MyBottomModal
 import com.kuit.ourmenu.ui.my.component.MyMealTime
 import com.kuit.ourmenu.ui.theme.Neutral700
 import com.kuit.ourmenu.ui.theme.Neutral900
 import com.kuit.ourmenu.ui.theme.NeutralBlack
 import com.kuit.ourmenu.ui.theme.OurMenuTypography
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyScreen() {
-    Scaffold(
-        topBar = {
-            OurMenuAddButtonTopAppBar(
+    var bottomSheetVisible by remember { mutableStateOf(false) }
+
+    Box{
+        Scaffold(
+            topBar = {
+                OurMenuAddButtonTopAppBar(
+                    modifier = Modifier
+                        .drawBehind {
+                            drawRect(
+                                color = NeutralBlack
+                            )
+                        }
+                        .shadow(elevation = 4.dp)
+                )
+            }
+        ) { innerPadding ->
+            Column(
                 modifier = Modifier
-                    .drawBehind {
-                        drawRect(
-                            color = NeutralBlack
-                        )
-                    }
-                    .shadow(elevation = 4.dp)
-            )
-        }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-        ) {
-            Row(
-                modifier = Modifier
-                    .padding(start = 20.dp, end = 20.dp, top = 32.dp, bottom = 24.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
+                    .padding(innerPadding)
             ) {
                 Row(
+                    modifier = Modifier
+                        .padding(start = 20.dp, end = 20.dp, top = 32.dp, bottom = 24.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_profile),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(44.dp),
+                            tint = Color.Unspecified
+                        )
+
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        Text(
+                            text = stringResource(R.string.email),
+                            style = OurMenuTypography().pretendard_700_14,
+                            color = Neutral900
+                        )
+                    }
+
                     Icon(
-                        painter = painterResource(R.drawable.ic_profile),
+                        painter = painterResource(R.drawable.ic_kebab),
                         contentDescription = null,
                         modifier = Modifier
-                            .size(44.dp),
-                        tint = Color.Unspecified
+                            .size(24.dp)
+                            .clickable(
+                                onClick = {
+                                    bottomSheetVisible = true
+                                }
+                            ),
+                        tint = Neutral700
                     )
+                }
 
-                    Spacer(modifier = Modifier.width(16.dp))
+                MyMealTime(
+                    // TODO: mealTimes 데이터 받아오기
+                )
 
+                Column(
+                    modifier = Modifier
+                        .padding(top = 32.dp, start = 20.dp, end = 20.dp)
+                ) {
+                    InfoRow(infoTitle = stringResource(R.string.notice)) {
+                        // TODO: 공지사항 화면으로 이동
+                    }
+                    Spacer(Modifier.height(20.dp))
+                    InfoRow(infoTitle = stringResource(R.string.customer_service)) {
+                        // TODO: 공지사항 화면으로 이동
+                    }
+                    Spacer(Modifier.height(20.dp))
+                    InfoRow(infoTitle = stringResource(R.string.app_review)) {
+                        // TODO: 공지사항 화면으로 이동
+                    }
+                    Spacer(Modifier.height(20.dp))
                     Text(
-                        text = stringResource(R.string.email),
-                        style = OurMenuTypography().pretendard_700_14,
+                        text = stringResource(R.string.app_version, "2.0"),
+                        style = OurMenuTypography().pretendard_600_16,
                         color = Neutral900
                     )
                 }
-
-                Icon(
-                    painter = painterResource(R.drawable.ic_kebab),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(24.dp)
-                        .clickable(
-                            onClick = {
-                                // TODO: 모달 만들기
-                            }
-                        ),
-                    tint = Neutral700
-                )
-            }
-
-            MyMealTime(
-                // TODO: mealTimes 데이터 받아오기
-            )
-
-            Column(
-                modifier = Modifier
-                    .padding(top = 32.dp, start = 20.dp, end = 20.dp)
-            ) {
-                InfoRow(infoTitle = stringResource(R.string.notice)) {
-                    // TODO: 공지사항 화면으로 이동
-                }
-                Spacer(Modifier.height(20.dp))
-                InfoRow(infoTitle = stringResource(R.string.customer_service)) {
-                    // TODO: 공지사항 화면으로 이동
-                }
-                Spacer(Modifier.height(20.dp))
-                InfoRow(infoTitle = stringResource(R.string.app_review)) {
-                    // TODO: 공지사항 화면으로 이동
-                }
-                Spacer(Modifier.height(20.dp))
-                Text(
-                    text = stringResource(R.string.app_version, "2.0"),
-                    style = OurMenuTypography().pretendard_600_16,
-                    color = Neutral900
-                )
             }
         }
 
+        if (bottomSheetVisible) {
+            MyBottomModal(
+                onDismissRequest = { bottomSheetVisible = false },
+                onChangePassword = { /* TODO */ },
+                onLogout = { /* TODO */ },
+                onDeleteAccount = { /* TODO */ }
+            )
+        }
     }
 }
 
