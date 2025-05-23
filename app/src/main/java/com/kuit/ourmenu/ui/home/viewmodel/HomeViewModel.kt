560e0bc7a2abee68b1262ce01650a8fb3585057f
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-05-23 15:48:36 +0900

    [feat]: home viewmodel 작성 (#64)

---
+++ app/src/main/java/com/kuit/ourmenu/ui/home/viewmodel/HomeViewModel.kt
@@ -0,0 +1,51 @@
+package com.kuit.ourmenu.ui.home.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kuit.ourmenu.data.model.home.response.HomeResponse
+import com.kuit.ourmenu.data.repository.HomeRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val homeRepository: HomeRepository
+): ViewModel() {
+
+    private val _home = MutableStateFlow(HomeResponse())
+    val home = _home.asStateFlow()
+
+    private val _error: MutableStateFlow<String?> = MutableStateFlow(null)
+    val error = _error.asStateFlow()
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading = _isLoading.asStateFlow()
+
+    init {
+        getHome()
+    }
+
+    fun getHome() {
+        viewModelScope.launch {
+            _isLoading.value = true
+            _error.value = null
+
+            homeRepository.getHome()
+                .fold(
+                    onSuccess = { response ->
+                        if (response != null) {
+                            _home.value = response
+                        }
+                    },
+                    onFailure = { throwable ->
+                        _error.value = throwable.message ?: "홈을 불러오는 중 오류가 발생했습니다."
+                    }
+                )
+
+            _isLoading.value = false
+        }
+    }
+}
