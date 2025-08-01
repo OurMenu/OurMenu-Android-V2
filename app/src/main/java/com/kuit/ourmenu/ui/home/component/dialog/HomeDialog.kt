845448455f3faee6a04d3b2e92216c649c3bc88d
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-08-01 18:30:23 +0900

    [refactor]: home dialog 수정 (#64)

--- app/src/main/java/com/kuit/ourmenu/ui/home/component/dialog/HomeDialog.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/home/component/dialog/HomeDialog.kt
@@ -1,24 +1,34 @@
 package com.kuit.ourmenu.ui.home.component.dialog
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import com.kuit.ourmenu.ui.common.dialog.DialogBigButton
+import coil3.compose.SubcomposeAsyncImage
+import com.kuit.ourmenu.R
+import com.kuit.ourmenu.data.model.home.response.Answer
+import com.kuit.ourmenu.data.model.home.response.HomeQuestionResponse
 import com.kuit.ourmenu.ui.theme.Neutral500
 import com.kuit.ourmenu.ui.theme.Neutral900
 import com.kuit.ourmenu.ui.theme.NeutralWhite
@@ -27,9 +37,10 @@ import com.kuit.ourmenu.ui.theme.ourMenuTypography
 
 @Composable
 fun HomePopUpDialog(
+    questionData: HomeQuestionResponse,
     onDismissRequest: () -> Unit,
-    onPositiveClick: () -> Unit,
-    onNegativeClick: () -> Unit
+    onAnswerSelected: (String) -> Unit,
+    onDiceClick: () -> Unit,
 ) {
     Dialog(onDismissRequest = onDismissRequest) {
         Surface(
@@ -56,17 +67,14 @@ fun HomePopUpDialog(
 
             ) {
                 HomeDialogAssets(
-                    onDiceClick = {
-                        // TODO : Dice Click Event
-                    },
+                    onDiceClick = onDiceClick,
                     onCloseClick = onDismissRequest
                 )
                 Text(
                     modifier = Modifier
                         .padding(top = 13.dp)
                         .fillMaxWidth(),
-                    text = "안녕하세요!\n" +
-                            "오늘의 기분은 어떠신가요?", // TODO : 추천 문구 반영
+                    text = stringResource(R.string.hello) + questionData.question,
                     style = ourMenuTypography().pretendard_700_20.copy(
                         color = Neutral900
                     ),
@@ -76,35 +84,44 @@ fun HomePopUpDialog(
                     modifier = Modifier
                         .padding(top = 13.dp, bottom = 19.dp)
                         .fillMaxWidth(),
-                    text = "질문에 답해 기분에 맞는 메뉴를\n" +
-                            "추천받아보세요!",
+                    text = stringResource(R.string.home_recommend),
                     style = ourMenuTypography().pretendard_500_14.copy(
                         fontWeight = FontWeight(500),
                         color = Neutral500
                     ),
                     textAlign = TextAlign.Center
                 )
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    questionData.answers.forEach { answer ->
+                        SubcomposeAsyncImage(
+                            model = answer.answerImgUrl,
+                            contentDescription = answer.answer,
+                            modifier = Modifier
+                                .width(248.dp)
+                                .height(48.dp)
+                                .clickable { onAnswerSelected(answer.answer) },
+                            loading = {
+                                Button(
+                                    modifier = Modifier
+                                        .width(248.dp)
+                                        .height(48.dp),
+                                    shape = RoundedCornerShape(8.dp),
+                                    colors = ButtonDefaults.buttonColors(
+                                        containerColor = Primary500Main,
+                                        contentColor = NeutralWhite
+                                    ),
+                                    onClick = {}
+                                ) {
 
-                DialogBigButton(
-                    modifier = Modifier
-                        .padding(bottom = 8.dp)
-                        .padding(horizontal = 4.dp)
-                        .fillMaxWidth()
-                        .height(48.dp),
-                    buttonText = "좋아!",
-                    containerColor = Primary500Main,
-                    onClick = onPositiveClick
-                )
-
-                DialogBigButton(
-                    modifier = Modifier
-                        .padding(horizontal = 4.dp)
-                        .fillMaxWidth()
-                        .height(48.dp),
-                    buttonText = "별로야..",
-                    containerColor = Primary500Main,
-                    onClick = onNegativeClick
-                )
+                                }
+                            },
+                        )
+                    }
+                }
             }
         }
     }
@@ -117,8 +134,21 @@ fun HomePopUpDialog(
 @Composable
 private fun HomePopUpDialogPreview() {
     HomePopUpDialog(
+        questionData = HomeQuestionResponse(
+            question = "스트레스 받을 때는 어떤 음식을 드시나요?",
+            answers = listOf(
+                Answer(
+                    "SWEET",
+                    "https://ourmenu-s3-default.s3.ap-northeast-2.amazonaws.com/answers/SWEET.svg"
+                ),
+                Answer(
+                    "SPICY",
+                    "https://ourmenu-s3-default.s3.ap-northeast-2.amazonaws.com/answers/SPICY.svg"
+                ),
+            )
+        ),
         onDismissRequest = {},
-        onPositiveClick = {},
-        onNegativeClick = {}
+        onAnswerSelected = {},
+        onDiceClick = {}
     )
 }
