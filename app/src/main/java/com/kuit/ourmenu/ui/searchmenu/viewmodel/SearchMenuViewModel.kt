938ec5317e53faa1a8b1b34ef46828c9ab43b198
casperjr <hyunjw0515@gmail.com>
2025-08-20 18:54:20 +0900

    [FIX]: 핀 중복 표시 오류 수정

--- app/src/main/java/com/kuit/ourmenu/ui/searchmenu/viewmodel/SearchMenuViewModel.kt
+++ app/src/main/java/com/kuit/ourmenu/ui/searchmenu/viewmodel/SearchMenuViewModel.kt
@@ -320,29 +320,34 @@ class SearchMenuViewModel @Inject constructor(
 
     fun getMapMenuDetail(menuId: Long) {
         viewModelScope.launch {
-            getMyMenus()
-            val response = mapRepository.getMapMenuDetail(menuId)
-            response.onSuccess { menuDetail ->
-                Log.d("SearchMenuViewModel", "메뉴 상세 조회 성공: $menuDetail")
-                // 검색 기록에서 해당 menuId를 가진 항목 찾기
-                searchHistory.value?.find { it.menuId == menuId }?.let { historyItem ->
-                    Log.d("SearchMenuViewModel", "검색 기록에서 찾은 mapId: ${historyItem.mapId}")
-                    // _myMenus를 해당 mapId로 필터링
-                    _myMenus.value = _myMenus.value?.filter { menu ->
-                        menu.mapId == historyItem.mapId
-                    } ?: emptyList()
-                    
-                    // 해당 mapId를 활성화 상태로 설정
-                    _activeMapId.value = historyItem.mapId
-                    
-                    // 지도에 검색 결과 표시
-                    showSearchResultOnMap()
-                    
-                    // 메뉴 상세 정보를 바텀시트에 표시하기 위해 설정
-                    getMapDetail(historyItem.mapId)
+            // 먼저 전체 메뉴를 가져옴
+            val myMenusResponse = mapRepository.getMap()
+            myMenusResponse.onSuccess { menus ->
+                if (menus != null){
+                    val allMenus = menus
+                    val menuDetailResponse = mapRepository.getMapMenuDetail(menuId)
+                    menuDetailResponse.onSuccess { menuDetail ->
+                        Log.d("SearchMenuViewModel", "메뉴 상세 조회 성공: $menuDetail")
+                        // 검색 기록에서 해당 menuId를 가진 항목 찾기
+                        searchHistory.value?.find { it.menuId == menuId }?.let { historyItem ->
+                            Log.d("SearchMenuViewModel", "검색 기록에서 찾은 mapId: ${historyItem.mapId}")
+                            // 가져온 전체 메뉴에서 필터링
+                            _myMenus.value = allMenus.filter { menu ->
+                                menu.mapId == historyItem.mapId
+                            }
+                            // 해당 mapId를 활성화 상태로 설정
+                            _activeMapId.value = historyItem.mapId
+                            // 지도에 검색 결과 표시
+                            showSearchResultOnMap()
+                            // 메뉴 상세 정보를 바텀시트에 표시하기 위해 설정
+                            getMapDetail(historyItem.mapId)
+                        }
+                    }.onFailure {
+                        Log.d("SearchMenuViewModel", "메뉴 상세 조회 실패: ${it.message}")
+                    }
                 }
             }.onFailure {
-                Log.d("SearchMenuViewModel", "메뉴 상세 조회 실패: ${it.message}")
+                Log.d("SearchMenuViewModel", "내 메뉴 조회 실패: ${it.message}")
             }
         }
     }
@@ -350,15 +355,21 @@ class SearchMenuViewModel @Inject constructor(
     // 지도에 검색 결과 핀 추가
     fun showSearchResultOnMap() {
         clearMarkers()
-        _myMenus.value?.forEach { store ->
-            addMarker(store, store.mapId == _activeMapId.value)
-            Log.d(
-                "SearchMenuViewModel",
-                "mapId: ${store.mapId} lat: (${store.mapY}, long: ${store.mapX})"
-            )
+        _myMenus.value?.let { menus ->
+            if (menus.isNotEmpty()) {
+                menus.forEach { store ->
+                    addMarker(store, store.mapId == _activeMapId.value)
+                    Log.d(
+                        "SearchMenuViewModel",
+                        "mapId: ${store.mapId} lat: (${store.mapY}, long: ${store.mapX})"
+                    )
+                }
+                // 첫 번째 검색 결과로 카메라 이동 TODO: 현재 위치랑 가까운 결과로 이동
+                moveCamera(menus[0].mapY, menus[0].mapX)
+            } else {
+                Log.d("SearchMenuViewModel", "검색 결과가 없습니다.")
+            }
         }
-        // 첫 번째 검색 결과로 카메라 이동 TODO: 현재 위치랑 가까운 결과로 이동
-        _myMenus.value?.get(0)?.let { moveCamera(it.mapY, it.mapX) }
     }
 
     // 네이버맵 이동을 위한 가게명 조회
